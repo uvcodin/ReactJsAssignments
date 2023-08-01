767fea0b5f7bad2095f502e3dfcf32a0d4ca1623
codsim <129361543+uvcodin@users.noreply.github.com>
2023-08-01 12:28:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import Navbar from './Navbar';
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      {/* Add other components and content here */}
-    </div>
-  );
-};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
-export default App;
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>Counter: {count}</h1>
+        <button onClick={this.handleIncrement}>plus</button>
+        <button onClick={this.handleDecrement}>minus</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
